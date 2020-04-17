90e14c04a50b10f2310deedf5c6bd0678a6bee5f
Brandon Wilson <brandon@coil.com>
2020-04-17 11:12:00 -0500

    add base podius spec definition

--- pkg/apis/codius/v1/podius_types.go
+++ pkg/apis/codius/v1/podius_types.go
@@ -7,11 +7,75 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+type EnvVar struct {
+    // Name of the environment variable. Must be a C_IDENTIFIER.
+    Name string `json:"name" protobuf:"bytes,1,opt,name=name"`
+
+    // Variable references $(VAR_NAME) are expanded
+    // using the previous defined environment variables in the container and
+    // any service environment variables. If a variable cannot be resolved,
+    // the reference in the input string will be unchanged. The $(VAR_NAME)
+    // syntax can be escaped with a double $$, ie: $$(VAR_NAME). Escaped
+    // references will never be expanded, regardless of whether the variable
+    // exists or not.
+    Value string `json:"value" protobuf:"bytes,2,opt,name=value"`
+}
+
+type Container struct {
+    // Name of the container specified as a DNS_LABEL.
+    // Each container in a pod must have a unique name (DNS_LABEL).
+    // Cannot be updated.
+    Name string `json:"name" protobuf:"bytes,1,opt,name=name"`
+    // Docker image name.
+    // More info: https://kubernetes.io/docs/concepts/containers/images
+    Image string `json:"image" protobuf:"bytes,2,opt,name=image"`
+    // Entrypoint array. Not executed within a shell.
+    // The docker image's ENTRYPOINT is used if this is not provided.
+    // Variable references $(VAR_NAME) are expanded using the container's environment. If a variable
+    // cannot be resolved, the reference in the input string will be unchanged. The $(VAR_NAME) syntax
+    // can be escaped with a double $$, ie: $$(VAR_NAME). Escaped references will never be expanded,
+    // regardless of whether the variable exists or not.
+    // Cannot be updated.
+    // More info: https://kubernetes.io/docs/tasks/inject-data-application/define-command-argument-container/#running-a-command-in-a-shell
+    // +optional
+    Command []string `json:"command,omitempty" protobuf:"bytes,3,rep,name=command"`
+    // Arguments to the entrypoint.
+    // The docker image's CMD is used if this is not provided.
+    // Variable references $(VAR_NAME) are expanded using the container's environment. If a variable
+    // cannot be resolved, the reference in the input string will be unchanged. The $(VAR_NAME) syntax
+    // can be escaped with a double $$, ie: $$(VAR_NAME). Escaped references will never be expanded,
+    // regardless of whether the variable exists or not.
+    // Cannot be updated.
+    // More info: https://kubernetes.io/docs/tasks/inject-data-application/define-command-argument-container/#running-a-command-in-a-shell
+    // +optional
+    Args []string `json:"args,omitempty" protobuf:"bytes,4,rep,name=args"`
+    // Container's working directory.
+    // If not specified, the container runtime's default will be used, which
+    // might be configured in the container image.
+    // Cannot be updated.
+    // +optional
+    WorkingDir string `json:"workingDir,omitempty" protobuf:"bytes,5,opt,name=workingDir"`
+    // List of environment variables to set in the container.
+    // Cannot be updated.
+    // +optional
+    // +patchMergeKey=name
+    // +patchStrategy=merge
+    Env []EnvVar `json:"env,omitempty" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,6,rep,name=env"`
+}
+
 // PodiusSpec defines the desired state of Podius
 type PodiusSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// List of containers belonging to the pod.
+	// Containers cannot currently be added or removed.
+	// There must be at least one container in a Pod.
+	// Cannot be updated.
+	// +patchMergeKey=name
+	// +patchStrategy=merge
+	Containers []Container `json:"containers" patchStrategy:"merge" patchMergeKey:"name" protobuf:"bytes,1,rep,name=containers"`
 }
 
 // PodiusStatus defines the observed state of Podius
